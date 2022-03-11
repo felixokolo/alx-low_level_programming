13c8aea925845b171ac9bdfbe7dda7dbb1ae5ae2
felixokolo <pheelix.info@gmail.com>
2022-03-11 12:40:41 -0800

    addition of print alphabet 2

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+	putchar(c);
+	for (c = 6; c < 91; c++)
+	putchar(c);
+	putchar(10);
+	return (0);
+}
