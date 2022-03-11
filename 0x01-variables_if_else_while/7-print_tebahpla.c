42407008fa7fefc4462dcd6fb7833eeffd7f38ac
felixokolo <pheelix.info@gmail.com>
2022-03-11 13:34:41 -0800

    addition

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int c;
+
+	for (c = 122; c > 96; c--)
+	putchar(c);
+	putchar(10);
+	return (0);
+}
