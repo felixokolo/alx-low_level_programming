651e1ffc94ddacac52582401946723f3cb1e7767
felixokolo <pheelix.info@gmail.com>
2022-03-13 14:54:09 -0700

    addition

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,26 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int c1, c2;
+	int c1, c2, c3, c4;
 
 	
 	for (c1 = 0; c1 < 10; c1++)
 	{
 		for (c2 = 0; c2 < 10; c2++)
 		{
-			putchar(48 + c1);
-			putchar(48 + c2);
-			if (c1 == 9 && c2 == 9)
-			continue;
-			putchar(44);
-			putchar(32);
+			for (c3 = 0; c2 < 10; c++)
+			{
+				for (c4 = 0; c4 < 10; c++)
+				{
+					putchar(48 + c1);
+					putchar(48 + c2);
+					putchar(32);
+					putchar(48 + c3);
+					putchar(48 + c4);
+					if (c1 == c2 == c3 == c4 == 9)
+					continue;
+					putchar(44);
+					putchar(32);
 	
 		}
 	}
