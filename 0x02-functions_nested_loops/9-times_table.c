b082d9fbffeb566515b7b193b793a9baaf6b19f9
Okolo Chukwudi Felix <pheelix.info@gmail.com>
2022-03-15 23:11:59 +0100

    Update 9-times_table.c
    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,3 +23,30 @@ void times_table(void)
 		}
 	}
 }
+
+/**
+ * _printnum - prints an integer number
+ * @n: number to print
+ * Return: void
+ */
+void _printnum(int n)
+{
+	int tmp = abs(n), arr[10], i = 0;
+
+	if (n == 0)
+	_putchar('0');
+	else
+	while (tmp != 0)
+	{
+		arr[i] = tmp % 10;
+		tmp /= 10;
+		i++;
+	}
+	if (n < 0)
+	_putchar('-');
+	while (i > 0)
+	{
+		_putchar('0' + arr[i - 1]);
+		i--;
+	}
+}
