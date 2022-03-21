3a9f465bd63911d8b9636e868efe96638641c785
felixokolo <pheelix.info@gmail.com>
2022-03-21 04:25:45 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets a pointed value to 98
+ * @n: pointer to value
+ * Return: void
+ */
+
+void print_numbers(int *n)
+{
+	*n = 98;
+}
