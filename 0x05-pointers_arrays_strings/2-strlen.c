fc155d1aa7ab08b2e4c8ed6418288d5053d58203
felixokolo <pheelix.info@gmail.com>
2022-03-21 13:08:32 -0700

    additions

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string s
+ * @s: pointer to string to find length
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
