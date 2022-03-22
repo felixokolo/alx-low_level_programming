857128ff6b0a079acaea4f3c91d12ccb56c9536d
felixokolo <pheelix.info@gmail.com>
2022-03-22 00:55:43 -0700

    addition

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - Converts ascii string to integer
+ * @s: sting to convert to integer
+ * Return: converted integer
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
