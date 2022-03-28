c710758661da6ba6eb5dbea2634559c6e7ca7edd
felixokolo <pheelix.info@gmail.com>
2022-03-28 14:30:40 -0700

    correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
  * Return: pointer to dest which golds concatenated string
  */
 
-char *_memcpy(char *dest, char src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
