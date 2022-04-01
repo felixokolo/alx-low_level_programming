6fb057f94342fb6e8159c20d5a8452f8d9fd7713
Okolo Chukwudi Felix <pheelix.info@gmail.com>
2022-04-01 23:53:24 +0100

    Update 0-whatsmyname.c
    Correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,17 +10,7 @@
  */
 
 /* betty style doc for function main goes there */
-int main(void)
+int main(int argc, char *argv[])
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	printf("%d is negative\n", n);
-	if (n == 0)
-	printf("%d is zero\n", n);
-	if (n > 0)
-	printf("%d is positive\n", n);
-	return (0);
+	printf("%s\n", argv[0]);
 }
