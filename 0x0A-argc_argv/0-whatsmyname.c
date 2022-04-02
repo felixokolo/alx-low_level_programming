bfa7a57c0248d93c73d1c73058931fc3c67f7f41
felixokolo <pheelix.info@gmail.com>
2022-04-01 20:17:29 -0700

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/* more headers goes there */
 
 /**
  * main - Entry point
  * @argc: holds number of variables
- * @argv: holds command strings 
+ * @argv: holds command strings
  * Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/* more headers goes there */
 
 /**
  * main - Entry point
  * @argc: holds number of variables
- * @argv: holds command strings 
+ * @argv: holds command strings
  * Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
 int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
