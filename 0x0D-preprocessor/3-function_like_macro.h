495f166d13226acf22dd10fc909f0f11326438b8
felixokolo <pheelix.info@gmail.com>
2022-04-11 07:04:20 -0700

    First commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_GUARD
+#define INCLUDE_GUARD
+
+#define ABS(x) x > 0 ? (x) : (x * (-1))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_GUARD
+#define INCLUDE_GUARD
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
