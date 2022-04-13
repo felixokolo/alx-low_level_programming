67237eaae5edf9ef7fc2adf0d60d03615a018ee9
felixokolo <pheelix.info@gmail.com>
2022-04-13 05:33:43 -0700

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - Function that prints a name
+ * @name: Name to be printed
+ * @f: Callback function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f();
+}
