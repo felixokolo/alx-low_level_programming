55303db74485450cb0d79ad16b1882dc7eaa4f51
felixokolo <pheelix.info@gmail.com>
2022-04-14 04:57:22 -0700

    addition

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: holds number of variables
+ * @argv: holds command strings
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+        int a, i;
+        int (*fun)(int, char **);
+
+	fun = main;
+	a = atoi(argv[1]);
+
+        if (argc != 2)
+        {
+            exit(98);
+        }
+
+        for (i = 0; i < a; i++)
+        {
+        	printf("%.2x", *(unsigned char *) (fun + i));
+		if (i == a)
+		continue;
+		printf(" ");
+	}
+	printf("\n");
+        return (0);
+}
