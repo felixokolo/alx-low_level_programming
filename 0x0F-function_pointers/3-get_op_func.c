7a28f821fd565c4d98c1e64fffc42fb370283758
Okolo Chukwudi Felix <pheelix.info@gmail.com>
2022-04-14 01:14:04 +0100

    correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,18 +13,18 @@ op_t ops[] = {
         {"+", op_add},
         {"-", op_sub},
         {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
-    
-    while (i < 5)
-    {
-    	if (*((ops[i]).op) == *s)
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+	
+	while (i < 5)
+	{
+		if (*((ops[i]).op) == *s)
 	return *ops[i].f;
 	i++;
-    }
+	}
 
-    return (NULL);
+	return (NULL);
 }
