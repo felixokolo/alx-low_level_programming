07c0e74a3121eca31c243474995eef8a8636cdc6
felixokolo <pheelix.info@gmail.com>
2022-04-14 15:14:12 -0700

    correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,6 +20,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(ap, char *);
+		if (s == NULL)
+		printf("(nil)");
+		else
 		printf("%s", s);
 		if (i == n - 1)
 		continue;
