7d2db875ff9706c0acc18f5e3b9d7f652daf0172
Okolo Chukwudi Felix <pheelix.info@gmail.com>
2022-04-14 01:20:21 +0100

    correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,20 +10,20 @@
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i = 0;
-	
+
 	while (i < 5)
 	{
 		if (*((ops[i]).op) == *s)
-	return *ops[i].f;
-	i++;
+		return *ops[i].f;
+		i++;
 	}
 
 	return (NULL);
