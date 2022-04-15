41dcf770f0de8dc1cd3e9f6c83f71fa4f0cc721c
felixokolo <pheelix.info@gmail.com>
2022-04-15 13:01:17 -0700

    correction

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,13 +11,57 @@
 
 void print_all(const char * const format, ...)
 {
-	unsigned int n;
+	unsigned int n, len;
+	char *s = "cifs", *sp = "cdfs";
 	va_list ap;
+	void (*funcs[])(void *c
 
 	va_start(ap, format);
-	n = strlen(format);
-	n++;
+	len = strlen(format);
+	n = 0;
+
+	while (*(format + n) != '\0')
+	{
+		if (check_format(*(format + n), s) >= 0)
+		{
+			
+
+
 
 	va_end(ap);
 	printf("\n");
+}
+
+/**
+ * check_format - checks if character is a valid specifier
+ * @c: character to check
+ * Return: 1 or 0
+ */
+
+int check_format(char c, char *s)
+{
+	int n, len;
+
+	len = strlen(s);
+	n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		if (c == *(s + n))
+		return (n);
+
+		n++;
+	}
+	return (-1);
+}
+
+/**
+ * print_char - prints character
+ * @ap: character to be printed
+ * Return: void
+ */
+
+void print_char(va_list *ap)
+{
+	printf("%c ", va_arg(*ap, char);
 }
