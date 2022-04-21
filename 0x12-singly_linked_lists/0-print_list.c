32febeb44fc1962f2079a337f528f285d8804b22
felixokolo <pheelix.info@gmail.com>
2022-04-21 04:57:18 -0700

    correction to task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,3 +1,5 @@
+#include "lists.h"
+
 /**
  * print_list -  function that prints
  * all the elements of a list_t
@@ -8,13 +10,14 @@
 size_t print_list(const list_t *h)
 {
 	unsigned int n;
-	list_t *tmp;
+	const list_t *tmp;
 
-	n = 1;
-	tmp = h->next;
+	n = 0;
+	tmp = h;
 
 	while (tmp != NULL)
 	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
 		tmp = tmp->next;
 		n++;
 	}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #ifndef CHECKER
 #define CHECKER
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -14,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
