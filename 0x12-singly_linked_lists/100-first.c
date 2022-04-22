4435308dacd4678e1f871a6215d9eae3e42fda86
felixokolo <pheelix.info@gmail.com>
2022-04-22 11:46:30 +0100

    addition of task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#inclide <stdio.h>
+
+void _start(void) __attribute__ ((constructor));
+
+/**
+ * _start -  function that runs before main
+ * Return: void
+ */
+
+void _start(void)
+{
+	printf("You're beat! and yet, you must allow,\
+	I bore my house upon my back!\n");
+
+}
