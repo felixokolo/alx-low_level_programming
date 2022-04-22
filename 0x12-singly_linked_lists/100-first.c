d105bb3d6d64b3fbebb2dff6fd9907c15083c340
felixokolo <pheelix.info@gmail.com>
2022-04-22 03:59:59 -0700

    correction to task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
-void _start(void) __attribute__ ((constructor));
+void _start_fun(void) __attribute__ ((constructor));
 
 /**
- * _start -  function that runs before main
+ * _start_fun -  function that runs before main
  * Return: void
  */
 
-void _start(void)
+void _start_fun(void)
 {
-	printf("You're beat! and yet, you must allow,\
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 
 }
