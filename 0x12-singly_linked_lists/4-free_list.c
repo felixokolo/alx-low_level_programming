7049477bc2d2b81f01d98e3fe9833365e877461b
felixokolo <pheelix.info@gmail.com>
2022-04-22 07:29:24 -0700

    correction to task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,15 +10,15 @@
 void free_list(list_t *head)
 {
 	list_t *tmp = head;
+
 	if (tmp == NULL)
 	return;
-	
+
 	while (head != NULL)
 	{
 		tmp = head->next;
 		free_list_t(head);
 		head = tmp;
-		
 	}
 }
 
