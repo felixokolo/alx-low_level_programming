33627dd289ccb175947d46cb0b1b8fb3258c98fd
felixokolo <pheelix.info@gmail.com>
2022-04-26 23:53:38 +0100

    addition

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,5 +6,16 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	int i = 0;
+	listint_t tmp = (*head)->next;
 
+	(*head)->next = NULL;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		tmp2 = tmp->next;
+		tmp->next = *head;
+		tmp = tmp2;
+		
+	}
 }
