4d79cc3dde3aad68747eb1adcdcae3889ff4f034
felixokolo <pheelix.info@gmail.com>
2022-04-26 19:42:01 +0000

    correction to task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *tmp2;
 	int ret = -1;
 
-	if (head == NULL)	
+	if (head == NULL)
 	return (ret);
 	if (*head == NULL)
 	return (ret);
