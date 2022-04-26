15203bc9ac59f7507d5ce7193dd5069ff00ec741
felixokolo <pheelix.info@gmail.com>
2022-04-26 19:46:26 +0000

    correction to task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,6 +10,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp, *new;
 
+	if (head == NULL)
+	return (NULL);
 	if (*head == NULL)
 	return (NULL);
 	tmp = *head;
