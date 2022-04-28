c7ef2e1b86007a924f02de765d0629d93b4e417d
felixokolo <pheelix.info@gmail.com>
2022-04-28 17:23:54 +0100

    correction to task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,24 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 0x80000000, tmp = 0;
-	int c = 0;
-
-	tmp += n;
-	if (tmp == 0)
-		_putchar('0');
-	while (n > 0 || tmp > 0)
-	{
-		if (c == 0 && (i & tmp) > 0)
-			c = 1;
-		if (c)
-		{
-			if ((i & tmp) == i)
-				_putchar('1');
-			else
-				_putchar('0');
-			n = n >> 1;
-		}
-		tmp = tmp << 1;
-	}
+	if (n > 1)
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
