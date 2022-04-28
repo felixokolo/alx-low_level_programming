6c10a37408cc5466d646fc0a45d37b8c6b67f876
felixokolo <pheelix.info@gmail.com>
2022-04-28 17:06:41 +0100

    correction to task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,5 +12,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int check_bin(const char *s);
+int _putchar(char c);
 
 #endif
