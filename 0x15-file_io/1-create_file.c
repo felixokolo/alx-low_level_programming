909a1c6cfd58d7b12ae29dc1a2589a310c08e348
felixokolo <pheelix.info@gmail.com>
2022-05-02 19:42:26 +0100

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, w_size;
-	char buffer[BUFF_SIZE];
 
 	if (text_content == NULL)
 	return (-1);
@@ -24,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	return (1);
-	w_size = write(fd, filename, strlen(text_content));
+	w_size = write(fd, text_content, strlen(text_content));
 	if (w_size < 0)
 	return (-1);
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,12 +11,11 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w_size;
-	char buffer[BUFF_SIZE];
 
 	if (text_content == NULL)
 	return (-1);
 
-	fd = open(filename, O_APPEND | O_RDONLY, 600);
+	fd = open(filename, O_APPEND | O_WRONLY, 600);
 	if (fd < 0)
 	{
 		return (-1);
@@ -24,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	return (1);
-	w_size = write(fd, filename, strlen(text_content));
+	w_size = write(fd, text_content, strlen(text_content));
 	if (w_size < 0)
 	return (-1);
 
