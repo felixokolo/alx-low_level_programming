e5d7a830b71825435739fe3bede5b03c8a495987
felixokolo <pheelix.info@gmail.com>
2022-05-02 22:54:48 +0100

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,21 +7,68 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-	int fd_f, fd_t;
+	int fd_f, fd_t, size_r, size_w;
+	char buffer[BUFF_SIZE];
 
 	if (argc < 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_t = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 664);
 	if (fd_t < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
-		exit(99);
-	}
+	print_err("Error: Can't write to ", argv[2], 99);
+
+	fd_f = open(argv[1], O_RDONLY);
+	if (fd_f < 0)
+	print_err("Error: Can't read from file ", argv[1], 98);
+
 	
+	do
+	{
+		size_r = read(fd_f, buffer, BUFF_SIZE);
+		if (size_r < 0)
+		print_err("Error: Can't read from file ", argv[1], 98);
+		size_w = write(fd_t, buffer, size_r);
+		if (size_w < 0)
+		print_err("Error: Can't write to ", argv[2], 99);
+		
+	}while (size_r > 0);
+	close_fd(fd_f);
+	close_fd(fd_t);
+	return (0);
+}
+
+/**
+ * close_fd - close file descriptor
+ * @fd: file descriptor to close
+ * Return: void
+ */
+
+void close_fd(int fd)
+{
+	int closer;
+
+	closer = close(fd);
+	if (closer < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * print_err - print error
+ * @c: error to print
+ * @c: error to print
+ * @exi: exit code
+ * Return: void
+ */
+
+void print_err(char *c, char *d, int exi)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", c, d);
+	exit(exi);
 }
