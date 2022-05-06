8a121c25824bc24dbf9a1f1ab15ad3e40a5c57c4
felixokolo <pheelix.info@gmail.com>
2022-05-06 04:01:22 -0700

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,22 +20,22 @@ int main(int argc, char *argv[])
 	}
 	fd_t = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_t == -1)
-	print_err("Error: Can't write to", argv[2], 99);
+	print_err("Error: Can't write to", argv[2], 99, fd_t);
 
 	close_fd(fd_t);
 	fd_f = open(argv[1], O_RDONLY);
 	if (fd_f == -1)
-	print_err("Error: Can't read from file", argv[1], 98);
+	print_err("Error: Can't read from file", argv[1], 98, fd_f);
 	fd_t = open(argv[2], O_APPEND | O_WRONLY);
 	size_r = read(fd_f, buffer, BUFF_SIZE);
 
 	do {
 		size_w = write(fd_t, buffer, size_r);
 		if (size_w == -1)
-		print_err("Error: Can't write to", argv[2], 99);
+		print_err("Error: Can't write to", argv[2], 99, fd_t);
 		size_r = read(fd_f, buffer, BUFF_SIZE);
 		if (size_r == -1)
-		print_err("Error: Can't read from", argv[1], 98);
+		print_err("Error: Can't read from", argv[1], 98, fd_f);
 	} while (size_r > 0);
 	close_fd(fd_f);
 	close_fd(fd_t);
@@ -56,8 +56,6 @@ void close_fd(int fd)
 	if (closer == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		close_fd(fd_f);
-		close_fd(fd_t);
 		exit(100);
 	}
 }
@@ -67,11 +65,13 @@ void close_fd(int fd)
  * @c: error to print
  * @d: error to print
  * @exi: exit code
+ * @fd: file descriptor
  * Return: void
  */
 
-void print_err(char *c, char *d, int exi)
+void print_err(char *c, char *d, int exi, int fd)
 {
 	dprintf(STDERR_FILENO, "%s %s\n", c, d);
+	close_fd(fd);
 	exit(exi);
 }
