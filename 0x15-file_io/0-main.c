e013d1b51849771e4dc84f01469afe7008d80e9f
felixokolo <pheelix.info@gmail.com>
2022-05-06 08:36:54 +0100

    corrections

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -3,20 +3,23 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
+ * @ac: Argumetns counter
+ * @av: Arguments array
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
+	char *content = "Best School !";
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (ac < 2)
+	{
+		printf("Usage: %s filename [content]\n", av[0]);
+		return (1);
+	}
+	res = create_file(av[1], av[2] ? av[2] : content);
+	printf("%i\n", res);
+	return (0);
 }
