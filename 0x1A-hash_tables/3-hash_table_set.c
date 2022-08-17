07c7cdcdb3f168c15495a4afb0c6b9eb2731dd57
felixokolo <pheelix.info@gmail.com>
2022-08-17 10:24:25 +0100

    merge

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,26 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	return (0);
 
 	idx = key_index((unsigned char *) key, ht->size);
-<<<<<<< HEAD
-	new = hash_table_set(ht, key, idx);
-	if (new == NULL)
-	{
-		new = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-		return (0);
-		new->next = NULL;
-		new->key = malloc(sizeof(char) * strlen(key) + 1);
-		strcpy(new->key, key);
-		new->value = malloc(strlen(value) + 1);
-		strcpy(new->value, value);
-		ht->array[idx] = new;
-	}
-	else
-	{
-		ht->array[idx]->key = realloc(ht->array[idx]->value, strlen(value) + 1);
-		strcpy(ht->array[idx]->value, value);
-	}
-=======
 	new = hash_table_search(ht, key, idx);
 	if (new == NULL)
 	{
@@ -64,7 +44,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 		strcpy(new->value, value);
 	}
->>>>>>> afa28cf9def9524c051b299d14d3a03ae2a128f6
 
 	return (1);
 }
@@ -78,37 +57,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * Return: Pointer to node success or NULL otherwise
  */
 
-<<<<<<< HEAD
-hash_node_t *hash_table_set(hash_table_t *ht, const char *key, unsigned long int idx)
-=======
 hash_node_t *hash_table_search(hash_table_t *ht,
 		const char *key, unsigned long int idx)
->>>>>>> afa28cf9def9524c051b299d14d3a03ae2a128f6
 {
 	hash_node_t *tmp;
 
 	if (ht->array[idx] == NULL)
-<<<<<<< HEAD
-		return (NULL)
-=======
 		return (NULL);
->>>>>>> afa28cf9def9524c051b299d14d3a03ae2a128f6
 	tmp = ht->array[idx];
 	while (tmp)
 	{
 		if (strcmp(key, tmp->key) == 0)
 		{
 			return (tmp);
-<<<<<<< HEAD
-			break;
-=======
->>>>>>> afa28cf9def9524c051b299d14d3a03ae2a128f6
 		}
 		tmp = tmp->next;
 	}
 	return (NULL);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> afa28cf9def9524c051b299d14d3a03ae2a128f6
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,10 +42,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-<<<<<<< HEAD
-int hash_table_set(hash_table_t *ht, const char *key, unsigned long int idx);
-=======
 hash_node_t *hash_table_search(hash_table_t *ht, const char *key, unsigned long int idx);
->>>>>>> afa28cf9def9524c051b299d14d3a03ae2a128f6
 
 #endif
