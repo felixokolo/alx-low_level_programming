1683f17da52cc6372d22561eabb2dcecbcd7dfbb
felixokolo <pheelix.info@gmail.com>
2022-08-22 21:04:18 +0100

    corrections

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
+
 """
-	module for island_perimeter
+5. Island Perimeter
+Technical interview preparation:
+- You are not allowed to google anything
+- Whiteboard first
 """
 
 
 def island_perimeter(grid):
-	"""
-		returns the perimeter of the island described in grid
-	"""
-	perimeter = 0
-	for i in range(len(grid)):
-		for j in range(len(grid[i])):
-			if grid[i][j] == 1:
-				if i == 0 or grid[i - 1][j] == 0:
-		            perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-    return (perimeter)
+    """
+    [returns the perimeter of the island described in grid]
+    """
+    terrain = 1
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for v_value in range(height):
+        for h_value in range(width):
+            if grid[v_value][h_value] == terrain:
+                size += 1
+                if (h_value > 0 and grid[v_value][h_value - 1] == terrain):
+                    edges += 1
+                if (v_value > 0 and grid[v_value - 1][h_value] == terrain):
+                    edges += 1
+    return size * 4 - edges * 2
