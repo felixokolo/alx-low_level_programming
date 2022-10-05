1c416dab1aa9d7f2a43fedb92f459e4ce15edfdd
felixokolo <pheelix.info@gmail.com>
2022-10-05 21:41:33 +0000

    correction

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,22 +16,22 @@ int binary_search(int *array, size_t size, int value)
 	return (-1);
 	len = size;
 	printf("Searching in array: ");
-	for (c = 0; c < len; c++)
+	for (c = 0; c < len - 1; c++)
 	{
-		printf("%d,", array[c]);
+		printf("%d, ", array[c]);
 	}
-	printf("\n");
+	printf("%d\n", array[c]);
 	if (size == 1)
 	if (value == array[0])
 	return (0);
 	if (size > 1)
 	{
-		target = len / 2;
+		target = (len - 1) / 2;
 		if (value > array[target])
 		{
-			ret = binary_search(array + target, target, value);
-			if (ret > 0)
-			ret = ret + target + (len % 2);
+			ret = binary_search(array + target + 1, len - (target + 1), value);
+			if (ret >= 0)
+			ret = ret + target + 1;
 		}
 		else if (value < array[target])
 		ret = binary_search(array, target, value);
